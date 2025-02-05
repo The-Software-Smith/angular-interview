f74bc1bb36da2ef62b5c80976950af65c2457975
mdebosz-softwaresmith <maks@softwaresmith.io>
2025-02-05 21:39:43 +0200

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: '*',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
