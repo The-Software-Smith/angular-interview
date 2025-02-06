7fe456c2602b787974894f8791afd73100a042e6
mdebosz-softwaresmith <maks@softwaresmith.io>
2025-02-06 14:01:16 +0200

    feat(ui): add possible recommended solution

--- frontend/src/feature/pois/ui/poi-list/poi-list.component.ts
+++ frontend/src/feature/pois/ui/poi-list/poi-list.component.ts
@@ -1,20 +1,20 @@
-import { Component, inject, output, Signal, OnInit } from '@angular/core';
-import { PoisPageComponent } from '../../pois-page.component';
+import { Component, output, Signal, OnInit, input, ChangeDetectionStrategy } from '@angular/core';
 import { POI } from '../../../../data/model/poi.dto';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatCard } from '@angular/material/card';
 import { Observable } from 'rxjs';
-
+import { ScrollingModule } from '@angular/cdk/scrolling';
 @Component({
   selector: 'app-poi-list',
-  imports: [ReactiveFormsModule, MatFormField, MatInput, MatCard, MatLabel],
+  imports: [ReactiveFormsModule, MatFormField, MatInput, MatCard, MatLabel, ScrollingModule],
   templateUrl: './poi-list.component.html',
-  styleUrl: './poi-list.component.scss'
+  styleUrl: './poi-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PoiListComponent implements OnInit {
-  readonly pois: Signal<POI[]> = inject(PoisPageComponent).pois;
+  readonly pois = input.required<POI[]>();
 
   readonly search = output<Observable<string | null>>();
   readonly selectPoi = output<number>();
